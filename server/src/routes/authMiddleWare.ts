014c1faab7047f7e67f2c339e4a24c8204f15159
Ayush <ayushkadam1404.com>
2023-11-17 19:22:51 +0530

    added optional checking

--- server/src/routes/authMiddleWare.ts
+++ server/src/routes/authMiddleWare.ts
@@ -3,7 +3,7 @@ import { Request,Response,NextFunction } from "express";
 
 export const authenticatejwt=(req:Request,res:Response,next:NextFunction)=>{
     try{
-        const token = req.headers.authorization!.split(' ')[1];
+        const token = req.headers.authorization?.split(' ')[1];
         const {id}=jwt.verify(token,process.env.JWTPRIVATEKEY)
         req.headers.id=id;
         next()
